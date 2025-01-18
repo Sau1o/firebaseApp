581472c2f97c12790e3c49fb4b259e98d9f7015a
Saulo Coelho <coelho.saulo@gmail.com>
2025-01-18 18:29:24 -0300

    tried to install firebase. ERROR

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,21 @@
 import { Stack } from "expo-router";
+import {useEffect, useState} from "react"
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth'
 
 export default function RootLayout() {
+  const [initializing, setInitializing] = useState(true);
+  const [user,setUser] = useState<FirebaseAuthTypes.User | null>();
+
+  const onAuthStateChanged = (user:FirebaseAuthTypes.User | null) => {
+    console.log('onAuthStateChanged',user)
+    if(initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged)
+    return subscriber
+  },[])
+  
   return (
     <Stack>
       <Stack.Screen name="index"/>
